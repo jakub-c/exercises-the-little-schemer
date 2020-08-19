116fe72671cc5c3ed3b1e2c03c9499305f72b4c9
Jakub <nawias@gmail.com>
2020-08-20 00:16:51 +0200

    add chapter 2 code

--- 02.rkt
+++ 02.rkt
@@ -1,1 +1,10 @@
 #lang racket
+
+(provide member?)
+
+(define member?
+  (lambda (a lat)
+    (cond
+      ((null? lat) #f)
+      (else (or (eq? (car lat) a)
+                (member? a (cdr lat)))))))
